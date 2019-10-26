dd0d7a3b21a9dce60417bff2978dcbbafc64822a
Abhishekkumar1909 <abhimait1909@gmail.com>
2019-10-26 20:37:45 +0530

    Added routes and screens

---
+++ lib/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'colors_and_constatnts.dart';
+String hint;
+
+InputDecoration inputDecoration = InputDecoration(
+  hintText: hint,
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: primaryColor, width: 3.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
