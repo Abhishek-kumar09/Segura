02df83e462d7a6fb02b33a809ca0b05bfc28cf1f
Abhishek-kumar09 <abhimait1909@gmail.com>
2020-01-25 01:27:45 +0530

    Drawer Modifications

--- lib/Main Page/main_page.dart
+++ lib/Main Page/main_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:segura_manegerial/Custom%20Function%20And%20Widgets/Widgets.dart';
 import 'package:segura_manegerial/Login%20And%20Register/edit_profile.dart';
 import 'package:segura_manegerial/Main%20Page/my_orders.dart';
@@ -10,6 +11,93 @@ import 'package:segura_manegerial/Custom Function And Widgets/Functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:segura_manegerial/onpressedevents/firebaseauth.dart';
 
+// class MainPage extends StatefulWidget {
+//   MainPage({@required this.phone});
+//   final String phone;
+//   @override
+//   _MainPageState createState() => _MainPageState();
+// }
+
+// class _MainPageState extends State<MainPage>
+//     with SingleTickerProviderStateMixin {
+//   TabController _tabController;
+//   FirebaseUser appuser;
+//   @override
+//   void initState() {
+//     super.initState();
+//     _tabController = TabController(
+//       vsync: this,
+//       length: 2,
+//     )..addListener(() {});
+//     getUser();
+//   }
+
+//   void getUser() async {
+//     appuser = await AuthCheck.getUser();
+//     setState(() {});
+//   }
+
+//   void onSelected(choice) async{
+//   try{await FirebaseAuth.instance.signOut();
+//   Navigator.of(context).pushNamedAndRemoveUntil(loginScreen, (Route<dynamic> route)=>false);
+//   Fluttertoast.showToast(msg: "Signed Out");}
+//   catch (e){
+//     Fluttertoast.showToast(msg: "Error while SignOut");
+//   }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: GradientAppBar(
+//         gradient: LinearGradient(colors: [Colors.blue[800],darkblue],begin: Alignment.topLeft,end: Alignment.bottomRight),
+//         elevation: 10,
+//         // centerTitle: true,
+//         leading: Padding(          
+//           padding: const EdgeInsets.all(10.0),
+//           child: CircleAvatar(backgroundImage: AssetImage('assets/logo1.png'),backgroundColor: Colors.white,)
+//         ),
+//         actions: [
+//           PopupMenuButton<String>(
+//             onSelected: onSelected,
+//   itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+//         const PopupMenuItem<String>(
+//           value: "signOut",
+//           child: 
+//           RowWithIcon(icon: Icons.person_outline,text: ' Sign Out',iconSize: 16,colour: Colors.black,)),
+//   ],
+// )]   ,
+//         primary: true,
+//         title: Text('SEGURA OWNER',),
+//         bottom: TabBar(          
+//           tabs: <Widget>[
+//             Tab(child: Text('MY ORDERS')),
+//             Tab(child: Text('MYSELF'))
+//           ],
+//           indicatorColor: Colors.blue[700], controller: _tabController,
+//           indicatorSize: TabBarIndicatorSize.tab,
+//           indicatorWeight: 6,
+//           unselectedLabelColor: Colors.lightBlueAccent,
+//           unselectedLabelStyle:
+//               TextStyle(fontWeight: FontWeight.bold, shadows: [
+//             Shadow(
+//               color: Colors.black38,
+//               blurRadius: 2,
+//             )
+//           ]),
+//         ),
+//       ),
+//       body: TabBarView(
+//         controller: _tabController,
+//         children: <Widget>[
+//           MyOrders(phoneNumber: widget.phone,),
+//           ProfileBuider(widget.phone,appuser)         
+//         ],
+//       ),
+//     );
+//   }  
+// }
+
 class MainPage extends StatefulWidget {
   MainPage({@required this.phone});
   final String phone;
@@ -17,17 +105,16 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage>
-    with SingleTickerProviderStateMixin {
-  TabController _tabController;
+class _MainPageState extends State<MainPage> {
+  // TabController _tabController;
   FirebaseUser appuser;
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(
-      vsync: this,
-      length: 2,
-    )..addListener(() {});
+    // _tabController = TabController(
+    //   vsync: this,
+    //   length: 2,
+    // )..addListener(() {});
     getUser();
   }
 
@@ -36,67 +123,104 @@ class _MainPageState extends State<MainPage>
     setState(() {});
   }
 
-  void onSelected(choice) async{
-  try{await FirebaseAuth.instance.signOut();
-  Navigator.of(context).pushNamedAndRemoveUntil(loginScreen, (Route<dynamic> route)=>false);
-  Fluttertoast.showToast(msg: "Signed Out");}
-  catch (e){
-    Fluttertoast.showToast(msg: "Error while SignOut");
-  }
+  void onSelected() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      // Alert(context: context,title: "Text('data')");
+      Navigator.of(context).pushNamedAndRemoveUntil(
+          loginScreen, (Route<dynamic> route) => false);
+      Fluttertoast.showToast(msg: "Signed Out");
+    } catch (e) {
+      Fluttertoast.showToast(msg: "Error while SignOut");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: GradientAppBar(
-        gradient: LinearGradient(colors: [Colors.blue[800],darkblue],begin: Alignment.topLeft,end: Alignment.bottomRight),
+        gradient: LinearGradient(
+            colors: [Colors.blue[800], darkblue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
         elevation: 10,
-        // centerTitle: true,
-        leading: Padding(          
-          padding: const EdgeInsets.all(10.0),
-          child: CircleAvatar(backgroundImage: AssetImage('assets/logo1.png'),backgroundColor: Colors.white,)
-        ),
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: onSelected,
-  itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-        const PopupMenuItem<String>(
-          value: "signOut",
-          child: 
-          RowWithIcon(icon: Icons.person_outline,text: ' Sign Out',iconSize: 16,colour: Colors.black,)),
-  ],
-)]   ,
         primary: true,
-        title: Text('SEGURA OWNER',),
-        bottom: TabBar(          
-          tabs: <Widget>[
-            Tab(child: Text('MY ORDERS')),
-            Tab(child: Text('MYSELF'))
-          ],
-          indicatorColor: Colors.blue[700], controller: _tabController,
-          indicatorSize: TabBarIndicatorSize.tab,
-          indicatorWeight: 6,
-          unselectedLabelColor: Colors.lightBlueAccent,
-          unselectedLabelStyle:
-              TextStyle(fontWeight: FontWeight.bold, shadows: [
-            Shadow(
-              color: Colors.black38,
-              blurRadius: 2,
-            )
-          ]),
+        centerTitle: true,
+        title: Text(
+          'SEGURA OWNER'
         ),
       ),
-      body: TabBarView(
-        controller: _tabController,
-        children: <Widget>[
-          MyOrders(phoneNumber: widget.phone,),
-          ProfileBuider(widget.phone,appuser)         
-        ],
+      drawer: Drawer(
+        elevation: 20,
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        'assets/t1.jpg',
+                      ),
+                      fit: BoxFit.cover,
+                      colorFilter:
+                          ColorFilter.mode(Colors.black54, BlendMode.darken))),
+              accountEmail: Text(appuser.phoneNumber),
+              accountName: Text('Segura'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/seguraLight.jpeg',
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Orders'),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ProfileBuider(widget.phone, appuser)));
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text(
+                'Sign Out'
+              ),
+              onTap: () {
+                onSelected();
+              },
+            ),
+            Divider(),
+            ListTile(title: Text('FAQ')),
+            Divider(),
+            ListTile(
+              title: Text(
+                'Close'
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              trailing: Icon(
+                Icons.keyboard_backspace,
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: MyOrders(
+        phoneNumber: widget.phone,
       ),
     );
-  }  
+  }
 }
-
 
 
 
